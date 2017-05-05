8259909c32c57bfd139b97638e4b190b9416555f
Jiang anqing <jianganqing@JiangdeMac-mini.local>
2017-05-05 21:46:57 +0900

    LSA

---
+++ LSA.py
@@ -0,0 +1,49 @@
+import nltk
+import math
+import string
+import numpy as np
+import pandas as pd
+
+from nltk.corpus import stopwords
+from collections import Counter
+from nltk.stem.porter import  *
+from sklearn.feature_extraction.text import  TfidfVectorizer
+
+text =  "The Colt Python is a .357 Magnum caliber revolver formerly \
+manufactured by Colt's Manufacturing Company of Hartford, Connecticut. \
+It is sometimes referred to as a \"Combat Magnum\".[1] It was first introduced \
+in 1955, the same year as Smith &amp; Wesson's M29 .44 Magnum. The now discontinued \
+Colt Python targeted the premium revolver market segment. Some firearm \
+collectors and writers such as Jeff Cooper, Ian V. Hogg, Chuck Hawks, Leroy \
+Thompson, Renee Smeets and Martin Dougherty have described the Python as the \
+finest production revolver ever made."
+
+
+def get_tokens(text):
+    lower = text.lower()
+    remove_punctuation_map = dict((ord(char), None) for char in string.punctuation)
+    no_punctuation = lower.translate(remove_punctuation_map)
+    tokens = nltk.word_tokenize(no_punctuation)
+    return tokens
+
+def stem_tokens(tokens, stemmer):
+    stemmed = []
+    for item in tokens:
+        stemmed.append(stemmer.stem(item))
+    return stemmed
+
+def count_tokens(text):
+    tokens = get_tokens(text)
+    filtered = [w for w in tokens if not w in stopwords.words('english')]
+    stemmer = PorterStemmer()
+    stemmed = stem_tokens(filtered, stemmer)
+    count = Counter(stemmed)
+    return count
+
+
+def main():
+    print(count_tokens(text))
+
+
+if __name__ == "_main_":
+    main()
---
+++ TFIDF.py
@@ -0,0 +1,63 @@
+import nltk
+import math
+import string
+
+from nltk.corpus import stopwords
+from collections import Counter
+from nltk.stem.porter import  *
+from sklearn.feature_extraction.text import  TfidfVectorizer
+
+nltk.download('punkt')
+
+text = "The Colt Python is a .357 Magnum caliber revolver formerly \
+manufactured by Colt's Manufacturing Company of Hartford, Connecticut. \
+It is sometimes referred to as a \"Combat Magnum\".[1] It was first introduced \
+in 1955, the same year as Smith &amp; Wesson's M29 .44 Magnum. The now discontinued \
+Colt Python targeted the premium revolver market segment. Some firearm \
+collectors and writers such as Jeff Cooper, Ian V. Hogg, Chuck Hawks, Leroy \
+Thompson, Renee Smeets and Martin Dougherty have described the Python as the \
+finest production revolver ever made."
+
+def get_tokens(text):
+    lower = text.lower()
+    remove_punctuation_map = dict((ord(char), None) for char in string.punctuation)
+    no_punctuation = lower.translate(remove_punctuation_map)
+    tokens = nltk.word_tokenize(no_punctuation)
+    return tokens
+
+def stem_tokens(tokens, stemmer):
+    stemmed = []
+    for item in tokens:
+        stemmed.append(stemmer.stem(item))
+    return stemmed
+    
+def tf(word, count):
+    return count[word] / sum(count.values())
+
+def n_containing(word, count_list):
+    return sum(1 for count in count_list if word in count)
+
+def idf(word, count_list):
+    return math.log(len(count_list)) / (1 + n_containing(word, count_list))
+
+def tfidf(word, count, count_list):
+    return tf(word, count) * idf(word, count_list)
+
+def main():
+    tokens = get_tokens(text)
+    filtered = [w for w in tokens if not w in stopwords.words('english')]
+    stemmer = PorterStemmer()
+    stemmed = stem_tokens(filtered, stemmer)
+    count = Counter(stemmed)
+    print (count)
+    countlist = [count]
+    for i, count in enumerate(countlist):
+        print("Top words in document {}".format(i + 1))
+        scores = {word: tfidf(word, count, countlist) for word in count}
+        sorted_words = sorted(scores.items(), key = lambda x: x[1], reverse=True)
+        for word, score in sorted_words[:3]:
+            print("\tWord: {}, TF-IDF: {}".format(word, round(score, 5)))
+
+if __name__ == "__main__":
+    main()
+
